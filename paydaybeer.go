1cfcfa8edcad0d7afc36d572389405caf37542e8
Olve Sæther Hansen <olve@vimond.com>
2016-11-25 16:16:28 +0100

    First revision

---
+++ paydaybeer.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func main() {
+
+  //endOfMonth := now.EndOfMonth()
+  date := time.Now()
+  isPaydayWeek := (25 - date.Day()) < 7
+  isFriday := date.Weekday() == time.Friday
+
+
+  if (isPaydayWeek && isFriday){
+    fmt.Println("Yesss! 🍺    it is Pay 🍹 Day 🍸 Beer! 🍻")
+  } else {
+    fmt.Println("Back 😰to work... 👷🏼")
+  }
+
+}
